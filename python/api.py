9474733a24e6d1f2ac900209ad7b816d45789999
Joshua Pfosi <joshpfosi@gmail.com>
2015-02-08 14:29:59 -0500

    added initial file

---
+++ python/api.py
@@ -0,0 +1,17 @@
+# api.py
+# This file defines a rudimentary Python API for Hydroshare's iRODS instance
+
+def share_with_user(resource, user):
+  pass
+def unshare_with_user(resource, user):
+  pass
+def share_with_group(resource, group) :
+  pass
+def unshare_with_group(resource, group):
+  pass
+def add_to_group(group, user):
+  pass
+def remove_from_group(group, user):
+  pass
+def ls_held_resources():
+  pass
